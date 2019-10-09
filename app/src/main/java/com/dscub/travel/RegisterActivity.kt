ae1ce6af836cd8ced0f29e46208ff0a8b85f0acc
=allenbangai <=allenkamadje@gmail.com>
2019-10-09 22:00:52 +0100

    removing appbar and building up user experience btw activities

--- app/src/main/java/com/dscub/travel/RegisterActivity.kt
+++ app/src/main/java/com/dscub/travel/RegisterActivity.kt
@@ -1,12 +1,24 @@
 package com.dscub.travel
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_register.*
 
 class RegisterActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        sign_up.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        goto_login_activity.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
