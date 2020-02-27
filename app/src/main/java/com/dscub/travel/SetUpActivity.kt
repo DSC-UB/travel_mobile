d890ed21001b203fc80eb72d646c0da2827a8951
=allenbangai <=allenkamadje@gmail.com>
2020-02-27 16:47:52 +0100

    setup activity UI complete

--- app/src/main/java/com/dscub/travel/SetUpActivity.kt
+++ app/src/main/java/com/dscub/travel/SetUpActivity.kt
@@ -1,12 +1,19 @@
 package com.dscub.travel
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_set_up.*
 
 class SetUpActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_set_up)
+
+        save_info_button.setOnClickListener{
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
